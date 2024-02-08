882268ede2142dd86742dc3e56feb33b4463a1bc
Danil <raskol@duck.com>
2024-02-08 13:36:00 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import { Layout } from 'antd';
-
-const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-const contentStyle = {
-  textAlign: 'center',
-  minHeight: 'calc(100vh - 60px)',
-  color: '#fff',
-  backgroundColor: '#001529',
-};
-const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
-
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
 
 export default function App() {
   return (
     <Layout>
-      <Layout.Header style={headerStyle}>Header</Layout.Header>
+      <AppHeader />
       <Layout>
-        <Layout.Sider width="25%" style={siderStyle}>
-          Sider
-        </Layout.Sider>
-        <Layout.Content style={contentStyle}>Content</Layout.Content>
+        <AppSider />
+        <AppContent />
       </Layout>
     </Layout>
   )
