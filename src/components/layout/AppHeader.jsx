5f34a5329ff0d5cfdfa36f61b60ee437b108f508
Danil <raskol@duck.com>
2024-02-12 19:50:23 +0300

    space

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,18 +1,41 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space } from 'antd';
+import { cryptoData } from '../../data';
 
 const headerStyle = {
+  with: '100%',
   textAlign: 'center',
-  color: '#fff',
+  // backgroundColor: '#010b1ae8',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#01070fe8',
+  padding: '1rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
 };
 
+const options = cryptoData.result
+
+console.log(options)
+
 export default function AppHeader() {
-  return (<Layout.Header style={headerStyle}>Header</Layout.Header>)
+  return (<Layout.Header style={headerStyle}>
+    <Select
+      mode="multiple"
+      style={{
+        width: '200px',
+      }}
+      // placeholder="select one country"
+      defaultValue={[]}
+      // onChange={handleChange}
+      optionLabelProp="label"
+      options={options}
+      optionRender={(option) => (
+        <Space>
+          <span role="img" aria-label={option.symbol}>
+            {option.icon}
+          </span>
+          {option.name}
+        </Space>
+      )}
+    />
+  </Layout.Header>)
 }
-
-// тут что-то будет
-
-// tut tut
