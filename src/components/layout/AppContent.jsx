f8a121c959482b33591ca44501d728d64dc1f12d
Danil <raskol@duck.com>
2024-02-20 17:36:29 +0300

    graph

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,7 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+import PortfolioChart from './PortfolioChart';
+import AssetsTable from './AssetsTable';
 
 const contentStyle = {
   textAlign: 'center',
@@ -9,7 +12,18 @@ const contentStyle = {
 }
 
 export default function AppContent() {
+  const { assets, crypto } = useCrypto()
   return (
-    <Layout.Content style={contentStyle}>Content</Layout.Content>
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ color: '#fff', textAlign: 'left' }}>
+        Portfolio: {assets.map(asset => {
+          const coin = crypto.find(c => c.id === asset.id)
+          return asset.amount * coin.price
+        })
+          .reduce((acc, v) => (acc += v), 0).toFixed(2)}$
+      </Typography.Title>
+      <PortfolioChart />
+      <AssetsTable />
+    </Layout.Content>
   )
 }
